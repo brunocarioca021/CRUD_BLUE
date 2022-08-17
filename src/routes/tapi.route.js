fa6a4443f8d247a7269d3d4982f58d3763b25c27
Bruno Dutra <brunocarioca021@gmail.com>
2022-08-17 20:22:05 -0300

    :wrench: add swagger

--- src/routes/tapi.route.js
+++ src/routes/tapi.route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
 const tapiController = require('../controllers/tapi.controller');
 const { validId, validObjectBody } = require('../middlewares/tapioca.middleware');
@@ -13,5 +15,8 @@ router.post('/create', validObjectBody, tapiController.createTapiController);
 router.put('/update/:id', validId, validObjectBody, tapiController.updateTapiController);
 
 router.delete('/delete/:id', validId, tapiController.deleteTapiController);
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
