5a83641ec1a2d6f9d6103ed0060cbab5264c7304
Bruno Dutra <brunocarioca021@gmail.com>
2022-08-17 19:15:01 -0300

    :goal_net: add middleware

--- src/controllers/tapi.controller.js
+++ src/controllers/tapi.controller.js
@@ -5,10 +5,6 @@ const findTapiController = async (req, res) => {
 };
 
 const findTapiByIdController = async (req, res) => {
-  if (!req.params.id) {
-    return res.status(400).send({ message: 'Id não informado' });
-  }
-
   const selectTapi = await tapiService.findTapiByIdService(req.params.id);
 
   if (!selectTapi) {
@@ -19,32 +15,16 @@ const findTapiByIdController = async (req, res) => {
 };
 
 const createTapiController = async (req, res) => {
-  if (!req.body.sabor || !req.body.descricao || !req.body.preco) {
-    return res
-      .status(400)
-      .send({ message: 'Voc~e não preencheu todos os dados' });
-  }
   res.send(await tapiService.createTapiService(req.body));
 };
 
 const updateTapiController = async (req, res) => {
-  if (!req.params.id) {
-    return res.status(400).send({ message: 'Tapioca não encontrada' });
-  }
-
-  if (!req.body.sabor || !req.body.descricao || !req.body.preco) {
-    return res
-      .status(400)
-      .send({ message: 'Você não preencheu todos os dados' });
-  }
   res.send(await tapiService.updateTapiService(req.params.id, req.body));
 };
 
 const deleteTapiController = async (req, res) => {
-  if (!req.params.id) {
-    return res.status(400).send({ message: 'Id invalido, tente novamente' });
-  }
-  const tapioca = await tapiService.deleteTapiService(req.params.id);
+ const tapioca = await tapiService.deleteTapiService(req.params.id);
+ 
   if (!tapioca) {
     return res.status(400).send({ message: 'Tapioca não encontrada' });
   }
--- src/routes/tapi.route.js
+++ src/routes/tapi.route.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const tapiController = require('../controllers/tapi.controller');
+const { validId, validObjectBody } = require('../middlewares/tapioca.middleware');
 
 router.get('/tapi', tapiController.findTapiController);
 
-router.get('/tapi/:id', tapiController.findTapiByIdController);
+router.get('/tapi/:id', validId, tapiController.findTapiByIdController);
 
-router.post('/create', tapiController.createTapiController);
+router.post('/create', validObjectBody, tapiController.createTapiController);
 
-router.put('/update/:id', tapiController.updateTapiController);
+router.put('/update/:id', validId, validObjectBody, tapiController.updateTapiController);
 
-router.delete('/delete/:id', tapiController.deleteTapiController);
+router.delete('/delete/:id', validId, tapiController.deleteTapiController);
 
 module.exports = router;
